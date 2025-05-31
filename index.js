d313bfd897ca0d4e6b6a5bf69a82006ca8dfdbd9
AMOGOS90 <121773272+AMOGOS90@users.noreply.github.com>
2025-05-31 15:16:15 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const productRoutes = require("./routes/productRoutes");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use("/api/products", productRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Backend Sugi Parfume is running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
